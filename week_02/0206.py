e2cce8f9bcaef5fda17200abcd548e87ad034549
MikePolyakov <id2k1149@gmail.com>
2019-10-06 22:12:28 +0300

    week 02 6/10

---
+++ week_02/0206.py
@@ -0,0 +1,22 @@
+# слова для цифр
+x = int(input())
+if x == 1:
+    print('one')
+else:
+    if x == 2:
+        print('two')
+    else:
+        if x == 3:
+            print('three')
+        else:
+            print('other')
+
+y = int(input())
+if y == 1:
+    print('one')
+elif x == 2:
+    print('two')
+elif x == 3:
+    print('three')
+else:
+    print('other')
